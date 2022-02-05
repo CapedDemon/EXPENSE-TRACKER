b0c5dc88e534b3a3ddb655a93e0ea3ade42d4b38
Shreejan <dolaishreejan@gmail.com>
2022-02-05 22:00:47 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+#  EXPENSE TRACKER
+<a href="https://github.com/Shreejan-35/Classified"><img src="https://img.shields.io/badge/Language-python-green.svg" alt="Language"></a>
+<a href="https://github.com/Shreejan-35/Classified/blob/master/LICENSE"><img src="https://img.shields.io/badge/License-GNU-blue.svg" alt="License"></a>
+
+Expense Tracker is a very good tool to keep track of your expenseditures and the total money you saved.🤑🤑
+
+We at some point of our life have kept track of our expenses and the money saved. 📃 We all have used the traditional way of keeping our 
+record by writing our expenses in our **personal diary** 📃 which can be burnt out, the papers might cut into pieces ✂ or the 📜 diary
+got so old 🗑 that we need to write our expenses in another diary.🖋🖋 It is very time consuming also.⏳⌚
+
+*But it is the most safe way of keeping record. But the time has changed laptops and mobiles are takng over. Most of the time we are spending on computers only.*
+
+![](https://github.com/Shreejan-35/EXPENSE-TRACKER/blob/main/res/web.JPG)
+
+**SO I HAVE THOUGHT OF MAKING AN PYTHON APPLICATION TO KEEP TRACK OF YOUR IMPORTANT EXPENSES. I SAY IT IS A VERY HANDY TOOL.**
+
+# 🧐ABOUT EXPENSE TRACKER 🎊🎊
+- Python application
+- Command Line Application but the things are well managed
+- User friendly
+- Shows detail view of your expenses and also how much money you saved or how much is extra
+- Shows the graph view of your expenses
+- You can update and delete your expenses
+- You can insert new expenses
+- Uses database
+
+# 😎GETTING STARTED ✨🎏
+Working with expense tracker is pretty easy, if I don't day also, then also you all will do it.
+There are two ways of downloading expense tracker in your computer :-
+    - Git clone 
+    ```
+    git clone https://github.com/Shreejan-35/EXPENSE-TRACKER.git
+    ```
+    - Download the zip file 
+After this you can run the main.exe file directly.
+Or if you have python installed then first run this command - 
+```
+pip install requirements.txt
+```
+After this run - 
+```
+python main.py
+```
+OR
+```
+python3 main.py
+```
+That's all.
+Read the instruction carefully on your screen and you will be able to do it.🎉🎉
+
+# CONTRIBUTION
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## LICENSE
+GNU PUBLIC LICENSE
---
+++ main.py
@@ -0,0 +1,193 @@
+# importing the required  libraries
+import sqlite3
+import os
+from turtle import width
+import matplotlib.pyplot as plt
+
+def mainActivity():
+    conn = sqlite3.connect('expenses.db')
+    cur = conn.cursor()
+
+    # making the table
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS expenses (expense TEXT, expenditure TEXT, idate TEXT)
+        """)
+
+    conn.commit()
+
+    # saving the salary of the user
+    if (os.path.isfile('./monthlySalary.txt')) == False:
+        salary = input("Enter your salary: ")
+        with open('monthlySalary.txt', 'w') as f:
+            f.write(salary)
+            f.close()
+
+    else:
+        if (os.path.getsize('./monthlySalary.txt')) == 0:
+            salary = input("Enter your salary: ")
+            with open('monthlySalary.txt', 'w') as f:
+                f.write(salary)
+                f.close()
+    running = True
+
+    while running:
+
+        # main menu
+        print('"""\nI = INSERT NEW EXPENSE\nR = SHOWS ALL YOUR DATA WITH DETAILS\nU = FOR UPDATING THE VALUES IN YOUR DATABASE\nD = FOR DELETING THE VLAUES IN YOUR DATABASE\nC = CHANGE YOUR SALARY\nQ = QUIT\n"""')
+
+        choice = input()
+
+        # if the choice is to insert new expense
+        if choice == 'I':
+
+            # entry of data loop
+            entry = True
+            while entry:
+
+                expense = input(
+                    "PUT THE NAME OF YOUR EXPENDITURE OR FOR WHAT THING YOU HAVE SPENT YOUR MONEY: ")
+                expenditure = input("HOW MUCH YOU HAVE SPENT ?: ")
+                date = input(
+                    "Put the date when you spend your money (Format = dd/mm/yyyy): ")
+
+                cur.execute("""
+                INSERT INTO expenses (expense, expenditure, idate) VALUES (?, ?, ?)""", (expense, expenditure, date))
+                conn.commit()
+
+                entryChoice = input(
+                    "DO YOU STILL WANT TO ENTER DATA. TYPE 'Y' IF YOU WANT TO AND 'N' IF YOU WANT TO STOP: ")
+                if entryChoice == 'N':
+                    entry = False
+
+        # if user wants to change the salary
+        if choice == 'C':
+            newSalary = input("Enter your new salary: ")
+            with open('monthlySalary.txt', "w") as f:
+                f.write(newSalary)
+
+                f.close()
+            print("Salary changed\n")
+
+        # if user wants to see all the data in the command line only
+        if choice == 'R':
+
+            # extracting the salary
+            f = open('monthlySalary.txt', "r")
+            extSalary = int(f.read())
+            f.close()
+
+            print('\n')
+            print(20 * '*')
+            print("Your expenses are as follows:- \n\n")
+            recieveData = cur.execute("""
+                SELECT * FROM expenses
+                """)
+
+            # printing the expenditure and calculating the total expenditure and stroing the data in lists for plotting the graph
+            totalExpenditureSum = 0
+            index = 1
+            x_axis = []
+            y_axis = []
+            print('S.no  Reason/Expense/Service  Expenditure  Date\n')
+            for x in recieveData:
+                print(str(index) + '. ' + x[0] + ' -> ' + x[1] + ' -> ' + x[2])
+                totalExpenditureSum = totalExpenditureSum + int(x[1])
+                index += 1
+                x_axis.append(x[0])
+                y_axis.append(int(x[1]))
+
+            print('\n')
+
+            # printing the total salary and total money saved or extra spend
+            print("Total salary = " + str(extSalary))
+            if totalExpenditureSum <= extSalary:
+                print("Money Saved = " + str(extSalary - totalExpenditureSum))
+            else:
+                print("Extra Money Spend = " +
+                      str(totalExpenditureSum - extSalary))
+
+            print(20 * '*')
+            print('\n')
+            print("(See the graph for more clear vision of your expense.)")
+
+            # plotting th graph
+            # This is a bar graph 
+            # In the x-axis there are the names or the reasons of your expense
+            # In the y-axis there are the values of the expense
+            # The title of the graph is 'Expense View'
+            graphChoice = input("Do you want to see the graph(Y/N) - ")
+            if graphChoice == 'Y':
+                plt.bar(x_axis, y_axis, width=0.3)
+                plt.title("Expense View")
+                plt.xlabel("<- Expense Name ->")
+                plt.ylabel("<- Expenditures ->")
+                plt.show()
+
+
+        # if user want to update data from the database
+        if choice == 'U':
+            print("(You want to update your data. Theses are the steps to follow - )\n")
+            print("i. You need to first give which thing to change.\n")
+            print("ii. You then need to specify for which rowid or date or expense reason or expenditure you need to change and give its value\n\n")
+            
+            changeValue = input("Enter the value which you want to change (expense - For changing the expense reason, expenditure - For changing the expenditure or the amount, date - For changing the data) - ")
+            newvalue = input("Enter the new value: ")
+            changeValueID = input("Enter any one of the value as an identification for changing the value which you give before it(expense, expenditure, date)- ")
+            idvalue = input("Enter the value of the identification - ")
+                
+            if changeValue == "expense":
+                if changeValueID == "expenditure":
+                    cur.execute(
+                        """UPDATE expenses SET expense = ? where expenditure = ?""", (newvalue, idvalue))
+
+                elif changeValueID == "date":
+                    cur.execute(
+                        """UPDATE expenses SET expense = ? where idate = ?""", (newvalue, idvalue))
+ 
+            elif changeValue == "expenditure":
+                if changeValueID == "expense":
+                    cur.execute(
+                        """UPDATE expenses SET expenditure = ? where expense = ?""", (newvalue, idvalue))
+
+                if changeValueID == "date":
+                    cur.execute(
+                        """UPDATE expenses SET expenditure = ? where idate = ?""", (newvalue, idvalue))
+
+            elif changeValue == "date":
+                if changeValueID == "expenditure":
+                    cur.execute(
+                        """UPDATE expenses SET idate = ? where expenditure = ?""", (newvalue, idvalue))
+
+                if changeValueID == "expense":
+                    cur.execute(
+                        """UPDATE expenses SET idate = ? where expense = ?""", (newvalue, idvalue))
+
+            
+            conn.commit()
+
+        # if user wants to delete something
+        if choice == 'D':
+            print("Deleting means you will delete the whole one record. Suppose you give an identification for deletion as date then the whole row where the date \nalong with expense, expenditure will eb deleted\n")
+            identification = input("Enter one identity for deleting the record(expense, expenditure, date): ")
+            valueid = input("Enter the value of the identity: ")
+
+            cur.execute("""DELETE from expenses where expenditure = ?""", (valueid,))
+            conn.commit()
+
+        # quitting the main while loop
+        if choice == 'Q':
+            running = False
+
+
+def main():
+    # first check whether the database to store expenses is present or not
+    if (os.path.isfile('./expenses.db')) == False:
+        print('WELCOME TO EXPENSE TRACKER. HERE YOU CAN STORE YOUR EXPENSES AND KEEP A TRACK OF THEM.\n')
+                                                         
+        mainActivity()
+
+    else:
+        mainActivity()
+
+if __name__ == '__main__':
+    main()
