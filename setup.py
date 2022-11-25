3e0b2228707507004d50f3d5266b3e7cc69097d5
Shreejan <dolaishreejan@gmail.com>
2022-11-25 15:13:34 +0530

    Refactor the code

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="expense-tracker",
-    version="0.0.6",
+    version="2.0",
     author="Shreejan Dolai",
     author_email="dolaishreejan@gmail.com",
     description="Expense Tracker is a very good tool to keep track of your expenseditures and the total money you saved.🤑🤑",
