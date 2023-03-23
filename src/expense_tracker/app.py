45c5b43ad3e0c956a360dd68c41c64eee5d6ae9f
Shreejan <dolaishreejan@gmail.com>
2023-03-23 21:21:44 +0530

    App.py done

--- src/expense_tracker/app.py
+++ src/expense_tracker/app.py
@@ -1,8 +1,16 @@
 from flask import Flask, request, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/database.db'
+
+# I have kept the DATABASE_URL in .env file
+# it is gitignored for safety issues 
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+
 db = SQLAlchemy(app)
 
 # The user model
@@ -26,132 +34,161 @@ class Expenses(db.Model):
 # register route
 @app.route('/register', methods=['POST'])
 def register():
-    data = request.json
-    if User.query.filter_by(username=data['username']).first():
-        return jsonify({'message': 'User with this username already exists! Try Again'}), 400
+    if request.is_json:
+        data = request.get_json()
 
-
-    user = User(username=data['username'], password=data['password'])
-    db.session.add(user)
-    db.session.commit()
-
-    return jsonify({'message': 'User registered successfully! Press L to login'}), 200
+        if User.query.filter_by(username=data['username']).first():
+            return jsonify({'message': 'User with this username already exists! Try Again'})
+        
+        else:
+            newUser = User(username=data['username'], password=data['password'])
+            db.session.add(newUser)
+            db.session.commit()
+            return {"message": f"User {newUser.username} has been registered successfully."}
+    else:
+        return {"message": "The request payload is not in JSON format"}
 
 # login route
 @app.route("/login", methods=["POST"])
 def login():
-    data = request.json
-    user = User.query.filter_by(username=data['username'], password=data['password']).first()
+    if request.is_json:
+        data = request.get_json()
+        user = User.query.filter_by(username=data['username'], password=data['password']).first()
 
-    if user:
-        return jsonify({'message': 'Access Granted'}), 200
+        if user:
+            return jsonify({'message': 'Access Granted'}), 200
+        else:
+            return jsonify({'message': 'Access Denied'})
+        
     else:
-        return jsonify({'message': 'Access Denied'}), 401
+        return {"message": "The request payload is not in JSON format"}
 
 # route to create the expenses in the expense table 
 @app.route('/createExpenses', methods=['POST'])
 def createExpenses():
-    expensesData = request.get_json()
-    if 'expense' not in expensesData or 'expenditure' not in expensesData or 'date' not in expensesData:
-        return jsonify({'error': 'Missing fields'}), 400
-    newExpense = Expenses(username=expensesData["username"], expense=expensesData["expense"], expenditure=expensesData["expenditure"], date=expensesData["date"])
-    db.session.add(newExpense)
-    db.session.commit()
+    if request.is_json:
+        expensesData = request.get_json()
+        if 'expense' not in expensesData or 'expenditure' not in expensesData or 'date' not in expensesData:
+            return jsonify({'error': 'Missing fields'}) 
+        else:
+            newExpense = Expenses(username=expensesData["username"], expense=expensesData["expense"], expenditure=expensesData["expenditure"], date=expensesData["date"])
+            db.session.add(newExpense)
+            db.session.commit()
 
-    return jsonify({'message': 'Expense created successfully'}), 200
+            return jsonify({'message': 'Expense created successfully'}), 200
+    
+    else:
+        return {"message": "The request payload is not in JSON format"}
 
 # route to get the expenses
 @app.route('/showExpenses', methods=['POST','GET'])
 def getExpenses():
-    data = request.json
-    expenses = Expenses.query.filter_by(username=data["username"]).all()
+    if request.is_json:
+        data = request.get_json()
+        expenses = Expenses.query.filter_by(username=data["username"]).all()
     
-    expenseList = []
-    for expense in expenses:
-        expenseDict = {'expense': expense.expense, 'expenditure': expense.expenditure, 'date': expense.date}
-        expenseList.append(expenseDict)
+        expenseList = []
+        for expense in expenses:
+            expenseDict = {'expense': expense.expense, 'expenditure': expense.expenditure, 'date': expense.date}
+            expenseList.append(expenseDict)
+        
+        return jsonify({'expenses': expenseList}), 200
     
-    return jsonify({'expenses': expenseList}), 200
+    else:
+        return {"message": "The request payload is not in JSON format"}
 
 # route to delete the expenses
-@app.route('/deleteExpenses', methods=['POST','GET'])
+@app.route('/deleteExpenses', methods=['POST'])
 def delExpenses():
-    data = request.json
-    if data["identification1"] == "expense":
-        if data["identification2"] == "expenditure":
-            result = Expenses.query.filter_by(username=data["username"], expenditure=data["value2"], expense=data["value1"]).delete()
+    if request.is_json:
+        data = request.get_json()
+        if data["identification1"] == "expense":
+            if data["identification2"] == "expenditure":
+                result = Expenses.query.filter_by(username=data["username"], expenditure=data["value2"], expense=data["value1"]).delete()
 
-        if data['identification2'] == "date":
-            result = Expenses.query.filter_by(username=data["username"], date=data["value2"], expense=data["value1"]).delete()
+            if data['identification2'] == "date":
+                result = Expenses.query.filter_by(username=data["username"], date=data["value2"], expense=data["value1"]).delete()
 
-    elif data["identification1"] == "expenditure":
-        if data["identification2"] == "expense":
-            result = Expenses.query.filter_by(username=data["username"], expenditure=data["value1"], expense=data["value2"]).delete()
+        elif data["identification1"] == "expenditure":
+            if data["identification2"] == "expense":
+                result = Expenses.query.filter_by(username=data["username"], expenditure=data["value1"], expense=data["value2"]).delete()
 
-        if data['identification2'] == "date":
-            result = Expenses.query.filter_by(username=data["username"], date=data["value2"], expenditure=data["value1"]).delete()
+            if data['identification2'] == "date":
+                result = Expenses.query.filter_by(username=data["username"], date=data["value2"], expenditure=data["value1"]).delete()
 
-    if data["identification1"] == "date":
-        if data["identification2"] == "expenditure":
-            result = Expenses.query.filter_by(username=data["username"], expenditure=data["value2"], date=data["value1"]).delete()
+        if data["identification1"] == "date":
+            if data["identification2"] == "expenditure":
+                result = Expenses.query.filter_by(username=data["username"], expenditure=data["value2"], date=data["value1"]).delete()
 
-        if data['identification2'] == "expense":
-            result = Expenses.query.filter_by(username=data["username"], date=data["value1"], expense=data["value2"]).delete()
+            if data['identification2'] == "expense":
+                result = Expenses.query.filter_by(username=data["username"], date=data["value1"], expense=data["value2"]).delete()
 
-    if result:
-        db.session.commit()
-        return jsonify({'message': 'Successfully deleted'}), 200
+        if result:
+            db.session.commit()
+            return jsonify({'message': 'Successfully deleted'}), 200
+        else:
+            return jsonify({'message': 'Could not delete'}) 
+
     else:
-        return jsonify({'message': 'Could not delete'}), 400
+        return {"message": "The request payload is not in JSON format"}
+    
 
 # route for updating the records
-@app.route("/updateExpenses", methods=["GET", "POST"])
+@app.route("/updateExpenses", methods=["POST"])
 def updateExpense():
-    data = request.json
-    if data["identification"] == "expense":
-        if data["changed"] == "expenditure":
-            updated = Expenses.query.filter_by(username=data["username"], expense=data["value"]).update(dict(expenditure=data["changedValue"]))
-        if data["changed"] == "date":
-            updated = Expenses.query.filter_by(username=data["username"], expense=data["value"]).update(dict(date=data["changedValue"]))
+    if request.is_json:
+        data = request.get_json()
+        if data["identification"] == "expense":
+            if data["changed"] == "expenditure":
+                updated = Expenses.query.filter_by(username=data["username"], expense=data["value"]).update(dict(expenditure=data["changedValue"]))
+            if data["changed"] == "date":
+                updated = Expenses.query.filter_by(username=data["username"], expense=data["value"]).update(dict(date=data["changedValue"]))
 
-    elif data["identification"] == "data":
-        if data["changed"] == "expenditure":
-            updated = Expenses.query.filter_by(username=data["username"], date=data["value"]).update(dict(expenditure=data["changedValue"]))
-        if data["changed"] == "expense":
-            updated = Expenses.query.filter_by(username=data["username"], date=data["value"]).update(dict(expense=data["changedValue"]))
+        elif data["identification"] == "data":
+            if data["changed"] == "expenditure":
+                updated = Expenses.query.filter_by(username=data["username"], date=data["value"]).update(dict(expenditure=data["changedValue"]))
+            if data["changed"] == "expense":
+                updated = Expenses.query.filter_by(username=data["username"], date=data["value"]).update(dict(expense=data["changedValue"]))
 
-    elif data["identification"] == "expenditure":
-        if data["changed"] == "expenditure":
-            updated = Expenses.query.filter_by(username=data["username"], expenditure=data["value"]).update(dict(expense=data["changedValue"]))
-        if data["changed"] == "date":
-            updated = Expenses.query.filter_by(username=data["username"], expenditure=data["value"]).update(dict(date=data["changedValue"]))
+        elif data["identification"] == "expenditure":
+            if data["changed"] == "expenditure":
+                updated = Expenses.query.filter_by(username=data["username"], expenditure=data["value"]).update(dict(expense=data["changedValue"]))
+            if data["changed"] == "date":
+                updated = Expenses.query.filter_by(username=data["username"], expenditure=data["value"]).update(dict(date=data["changedValue"]))
 
-    if updated:
-        db.session.commit()
-        return jsonify({'message': 'Successfully updated'}), 200
+        if updated:
+            db.session.commit()
+            return jsonify({'message': 'Successfully updated'}), 200
+        else:
+            return jsonify({'message': 'Could not be updated'}) 
+        
     else:
-        return jsonify({'message': 'Could not be updated'}), 400
+        return {"message": "The request payload is not in JSON format"}
 
 # deleting your whole account
-@app.route("/deleteAccount", methods=["GET"])
+@app.route("/deleteAccount", methods=["POST", "GET"])
 def delAccount():
-    data = request.json
-    userExpense = Expenses.query.filter_by(username=data['username']).first()
+    if request.is_json:
+        data = request.get_json()
+        userExpense = Expenses.query.filter_by(username=data['username']).first()
 
-    if userExpense:
-        deleteExpense = Expenses.query.filter_by(username=data["username"]).delete()
+        if userExpense:
+            deleteExpense = Expenses.query.filter_by(username=data["username"]).delete()
 
-        if deleteExpense:
-            db.session.commit()
+            if deleteExpense:
+                db.session.commit()
+
+        else:
+            delUser = User.query.filter_by(username=data["username"]).delete()
+
+            if delUser:
+                db.session.commit()
+                return jsonify({'message': 'Successfully loggedout', 'del':delUser}), 200
+            else:
+                return jsonify({'message': 'Could not delete account', 'del':delUser})
 
     else:
-        delUser = User.query.filter_by(username=data["username"]).delete()
-
-        if delUser:
-            db.session.commit()
-            return jsonify({'message': 'Successfully loggedout', 'del':delUser}), 200
-        else:
-            return jsonify({'message': 'Could not delete account', 'del':delUser}), 400
+        return {"message": "The request payload is not in JSON format"} 
 
 
 @app.route("/")
@@ -159,5 +196,6 @@ def home():
     return render_template("index.html")
 
 if __name__ == '__main__':
-    db.create_all()
-    app.run(host = "0.0.0.0")
+    # db.drop_all()
+    # db.create_all()
+    app.run(host = "0.0.0.0", debug=True)
