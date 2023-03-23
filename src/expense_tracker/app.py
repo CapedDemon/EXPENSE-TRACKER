cd76e7ba5ac40d937427ca34f37db1efc679414c
Shreejan <dolaishreejan@gmail.com>
2023-03-23 21:27:30 +0530

    App.py done

--- src/expense_tracker/app.py
+++ src/expense_tracker/app.py
@@ -1,9 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 app = Flask(__name__)
 
